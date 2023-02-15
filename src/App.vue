e996cfd7c97eca1132eaba532eea8d61261c5d32
Json <jasonleung1394@gmail.com>
2023-02-16 04:45:58 +1100

    maintainance

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Constructing logo" class="logo" src="@/assets/constructing.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="This Page Is Still Under-development" />
     </div>
-    <nav>
+    <!-- <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
-    </nav>
+    </nav> -->
   </header>
 
   <RouterView />
